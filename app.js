0e80aacae188493198b47a021d7354151a2e1d96
Dan Choi <ygdanchoi@gmail.com>
2022-10-04 21:29:45 -0400

    randomize names

--- app.js
+++ app.js
@@ -1,44 +1,25 @@
 const express = require('express');
+const { uniqueNamesGenerator, names } = require('unique-names-generator');
+
 const app = express();
 const port = 3000;
 
 app.use((req, res, next) => setTimeout(next, 2000));
 
 app.get('/people', (req, res) => {
-  res.send(peopleResponse());
+  const response = [];
+
+  for (let i = 0; i < 30; i++) {
+    response.push({
+      name: uniqueNamesGenerator({
+        dictionaries: [names]
+      })
+    });
+  }
+
+  res.send(response);
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 });
-
-function peopleResponse() {
-  return [
-    { name: "anita" },
-    { name: "bertha" },
-    { name: "caleb" },
-    { name: "darcy" },
-    { name: "emilio" },
-    { name: "fabian" },
-    { name: "gustavo" },
-    { name: "hephaestus" },
-    { name: "ignacio" },
-    { name: "juarez" },
-    { name: "katerina" },
-    { name: "lennox" },
-    { name: "maximilian" },
-    { name: "napoleon" },
-    { name: "ophelia" },
-    { name: "paulina" },
-    { name: "quinn" },
-    { name: "rasputin" },
-    { name: "sophocles" },
-    { name: "tiberius" },
-    { name: "ursula" },
-    { name: "valentino" },
-    { name: "winston" },
-    { name: "xavier" },
-    { name: "yolanda" },
-    { name: "zechariah" },
-  ];
-}
