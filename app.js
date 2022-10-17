4aa81aa1c777bea174c947f087ea845203b609c8
Dan Choi <ygdanchoi@gmail.com>
2022-10-17 19:14:56 -0400

    add error handling later

--- app.js
+++ app.js
@@ -7,14 +7,16 @@ const port = 3000;
 app.use((req, res, next) => setTimeout(next, 1000));
 
 app.get('/people', (req, res) => {
-  if (Math.random() <= 0.8) { // error rate
+  if (Math.random() <= 1.0) { // error rate
     const response = [];
   
     for (let i = 0; i < 30; i++) {
       response.push({
         name: uniqueNamesGenerator({
           dictionaries: [names]
-        })
+        }),
+        age: Math.ceil(Math.random() * 100),
+        weight: Math.ceil(Math.random() * 300)
       });
     }
   
